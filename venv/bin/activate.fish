9bb51597cb173a07b6bdecabd86b7b9b311edbd7
ligade <sourabhligade07@gmail.com>
2024-02-11 12:03:18 +0530

    files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sourabhligade/wormyreads/venv"
+set -gx VIRTUAL_ENV "/Users/sourabhligade/Documents/wormyreadss/wormyreadss/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
